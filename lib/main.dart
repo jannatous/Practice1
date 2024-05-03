2513a62dcbda735ae8655f105ef5f0328bf242a1
USER <jannatous121@gmail.com>
2024-05-03 19:33:40 +0600

    GestureDetector,Inkwell

--- lib/main.dart
+++ lib/main.dart
@@ -27,77 +27,14 @@ class MyApp extends StatelessWidget{
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-
-            ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                    backgroundColor:Colors.purple,
-                    textStyle: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                    )
-                ),
-                onLongPress: (){
-                  print('Email deleted');
-                },
-                onPressed:(){
-                  print('Email has been sent');
-                  }, child: Text('Send Email')),
-            TextButton (
-              style: TextButton.styleFrom(
-                foregroundColor: Colors.amber,
-                textStyle: TextStyle(
-                  fontSize: 30,
-                )
-              ),
-                onPressed: (
-                ) {}, child:Text('Resend')),
-            IconButton(onPressed: (){}, icon:Icon(Icons.add)),
-            OutlinedButton(
-              style: TextButton.styleFrom(foregroundColor: Colors.green),
-                onPressed: () {}, child: Text('Outlined Button')),
-            TextField(
-              style: TextStyle(
-                color: Colors.cyan,
-              ),
-
-              decoration: InputDecoration(
-                fillColor: Colors.purple,
-                filled: true,
-                hintText: 'Enter your email address',
-                hintStyle: TextStyle( color:Colors.white),
-                  suffixIcon: Icon(Icons.email_outlined),
-                label: Text('Email address'),
-
-              ),
-
-            ),
-            SizedBox(
-              height: 10,
-              width: 10,
-            ),
-            TextField(
-              obscureText: true,
-              style: TextStyle(
-                color: Colors.cyan,
-              ),
-
-              decoration: InputDecoration(
-                fillColor: Colors.purple,
-                filled: true,
-                hintText: 'Enter your email address',
-                hintStyle: TextStyle( color:Colors.white),
-                suffixIcon: Icon(Icons.password),
-                label: Text('1234'),
-                border: OutlineInputBorder(),
-                enabledBorder:OutlineInputBorder(
-                  borderSide: BorderSide(color:Colors.white),
-                ),
-               // focusedBorder:OutlinedBorder(),
-
-              ),
-
-            ),
-
+          InkWell(
+            onTap: (){
+              print('tapped on Jannat');
+            },
+            borderRadius: BorderRadius.circular(16),
+            onLongPress: (){},
+            child: Text('Jannat',style:TextStyle(fontSize: 40)),
+          ),
           ],
 
         ),
