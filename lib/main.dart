115c33da4a5e77e7557f8ace64121c683f3e0095
USER <jannatous121@gmail.com>
2024-05-03 18:31:10 +0600

    Buttons,textfield,container,form

--- lib/main.dart
+++ lib/main.dart
@@ -23,62 +23,86 @@ class MyApp extends StatelessWidget{
           ],
         ),
 
-      //  body: Center(
-        // child: Text('Jannat',style: TextStyle(color: Colors.green,fontSize: 35,fontWeight:FontWeight.bold),),
-       // ),
+      body:Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
 
-      /*  body:Column(
-          children: [Text('Jannatul',style: TextStyle(color: Colors.green,fontSize: 35,fontWeight:FontWeight.bold),),
-            Text(' Ferdaous',style: TextStyle(color: Colors.green,fontSize: 35,fontWeight:FontWeight.bold),),
-            Icon(Icons.account_circle,size: 50,color: Colors.green,)
-          ],
-        ),
-       */
-     body: Center(
-       child: Column(
-         mainAxisAlignment: MainAxisAlignment.start,
-         children: [Text('Home',style: TextStyle(color: Colors.green,fontSize: 35,fontWeight:FontWeight.bold),),
-       Text(' Screen',style: TextStyle(color: Colors.green,fontSize: 35,fontWeight:FontWeight.bold),),
+            ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    backgroundColor:Colors.purple,
+                    textStyle: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    )
+                ),
+                onLongPress: (){
+                  print('Email deleted');
+                },
+                onPressed:(){
+                  print('Email has been sent');
+                  }, child: Text('Send Email')),
+            TextButton (
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.amber,
+                textStyle: TextStyle(
+                  fontSize: 30,
+                )
+              ),
+                onPressed: (
+                ) {}, child:Text('Resend')),
+            IconButton(onPressed: (){}, icon:Icon(Icons.add)),
+            OutlinedButton(
+              style: TextButton.styleFrom(foregroundColor: Colors.green),
+                onPressed: () {}, child: Text('Outlined Button')),
+            TextField(
+              style: TextStyle(
+                color: Colors.cyan,
+              ),
 
-           Row(
-             children: [
-               Text('Jannatul',style: TextStyle(color: Colors.green,fontSize: 35,fontWeight:FontWeight.bold),),
-               Text(' Ferdaous ',style: TextStyle(color: Colors.green,fontSize: 35,fontWeight:FontWeight.bold),),
-             ],
+              decoration: InputDecoration(
+                fillColor: Colors.purple,
+                filled: true,
+                hintText: 'Enter your email address',
+                hintStyle: TextStyle( color:Colors.white),
+                  suffixIcon: Icon(Icons.email_outlined),
+                label: Text('Email address'),
 
-           ),
-          Image.network('https://cdn.vectorstock.com/i/1000x1000/88/62/female-doll-cartoon-vector-48838862.webp',width:200,height:100),
-           Image.asset('images/me1.jpg',width:100,height:100),
-           RichText(text: TextSpan(
-             text: 'Hello',
-            style: TextStyle(
-                color:Colors.green
+              ),
+
+            ),
+            SizedBox(
+              height: 10,
+              width: 10,
+            ),
+            TextField(
+              obscureText: true,
+              style: TextStyle(
+                color: Colors.cyan,
+              ),
+
+              decoration: InputDecoration(
+                fillColor: Colors.purple,
+                filled: true,
+                hintText: 'Enter your email address',
+                hintStyle: TextStyle( color:Colors.white),
+                suffixIcon: Icon(Icons.password),
+                label: Text('1234'),
+                border: OutlineInputBorder(),
+                enabledBorder:OutlineInputBorder(
+                  borderSide: BorderSide(color:Colors.white),
+                ),
+               // focusedBorder:OutlinedBorder(),
+
+              ),
+
             ),
 
-             children: [
-               TextSpan(
-               text: ' Name ',
-           style: TextStyle(
-               color: Colors.blue
-           ),
-               ),
-  TextSpan(
-  text: ' I am',
-    style: TextStyle(
-      fontSize: 20,
-      fontWeight: FontWeight.bold
-    )
-),
-             ]
-           )
+          ],
 
-           )
+        ),
+      )
 
-         ]
-       ),
-       
-       
-     )
       ),
     );
   }
